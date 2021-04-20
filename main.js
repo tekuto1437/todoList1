cb72897daa25763c63970a45132ac9b2a9cfc01c
tekuto1437 <denisjerosenko79@gmail.com>
2021-04-20 15:21:04 +0300

    Save3

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 let addMessage = document.querySelector('.message'),
     addButton = document.querySelector('.add');
+    todo = document.querySelector('.todo');
+
+let todoList = [];
+
+addButton.addEventListener('click', function(){
+
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
+
+        todoList.push(newTodo);
+    });
+
+function displayMessages(){
+
+    todoList.forEach(function(item, i){
+        let displayMessage = `Our task: ${addMessage.value}`;
+        console.log('displayMessage: ', displayMessage)
+    });
+
+}
+
